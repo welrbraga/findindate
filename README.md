08efe73a4be801269b920b1cd44d498847129c75
Welington Rodrigues Braga <welrbraga@yahoo.com>
2018-12-05 18:31:45 -0200

    First public version
    Primeira versão publica do script.
    
    Signed-off-by: Welington Rodrigues Braga <welrbraga@yahoo.com>

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ If you have asked these question once, so this tool is for you.
 
 I have to manipulate lots of files everyday and one of the most common resource I use to filter the files is that 
 available by GNU find but work with dates is very hard so I had created this litle script to simplify these jobs.
+
+
+## INSTALL ##
+
+After clone this repo or just download de file "findindate.sh" execute these commands:
+
+$ chmod +x findindate.sh
+$ sudo ./findindate.sh --install
+
+
+## USAGE ##
+
+Check the output of "findindate --help" to get instructions.
+
---
+++ findindate.sh
@@ -0,0 +1,239 @@
+#!/bin/bash
+
+# Prepara os argumentos para o comando find localizar arquivos dentro de um dado intervalo de datas
+
+# Wbraga - 2018-11-27
+
+
+#Configura o find para localizar arquivos dentre duas datas
+between() {
+  touch -t "$1" /tmp/afterdate
+  touch -t "$2" /tmp/beforedate
+
+  echo  -newer /tmp/afterdate -and \! -newer /tmp/beforedate
+}
+
+
+#Configura o find para localizar arquivos antes da data informada
+before() {
+  touch -t "$1" /tmp/beforedate
+
+  echo  \! -newer /tmp/beforedate
+}
+
+#Configura o find para localizar arquivos apos a data informada
+after() {
+  touch -t "$1" /tmp/afterdate
+
+  echo  -newer /tmp/afterdate
+}
+
+#Valida a data passada a ser usada na funcao between()
+checkdate() {
+  case $MONTH in # Determina se é mes de 30 ou 31 dias
+    01 | 03 | 05 | 07 | 08 | 10 | 12 )
+      lastday=31;
+    ;;
+    04 | 06 | 09 | 11 )
+      lastday=30;
+    ;;
+    02 )
+      lastday=28;
+      if (( ${YEAR} % 4 == 0 )) && \
+         (( (( ${YEAR} % 100 != 0 )) || (( ${YEAR} % 400 == 0 )) ))  ; \
+         then lastday=29 ;
+      fi #Determina se e ano bisexto
+    ;;
+  esac
+
+  if (( $informyear == 1 )) && (( $informmonth == 1 )); then
+    DATA1=`date +${YEAR}${MONTH}010000`;
+    DATA2=`date +${YEAR}${MONTH}${lastday}2359.59`;
+  fi
+  if (( $informyear == 1 )) && (( $informmonth == 0 )); then
+    DATA1=`date +${YEAR}01010000`;
+    DATA2=`date +${YEAR}12312359.59`;
+  fi
+  if (( $informyear == 0 )) && (( $informmonth == 1 )); then
+     YEAR=`date +%Y`;
+     DATA1=`date +${YEAR}${MONTH}010000`;
+     DATA2=`date +${YEAR}${MONTH}${lastday}2359.59`;
+  fi
+}
+
+help() {
+echo "-false"
+cat <<EOF >&2
+    Find In Date - Auxilia na localização de arquivos por data usando o GNU Find
+
+    --after, -a [[CC]YY]MMDDhhmm[.ss]
+    --before, -b [[CC]YY]MMDDhhmm[.ss]
+      Arquivos modificados antes ou após a data/hora informada.
+
+      * A data é informada no mesmo formato do comando "touch -t"
+
+   --between, -w [[CC]YY]MMDDhhmm[.ss] [[CC]YY]MMDDhhmm[.ss]
+     Arquivos modificados dentro do intervalo de tempo informado.
+
+     * A data é informada no mesmo formato do comando "touch -t"
+
+    --today , -t
+      Arquivos modificados hoje
+
+    --yesterday, -y
+      Arquivos modificados ontem
+
+    --inmonth, -m [01-12]
+      Arquivos modificados no mês informado.
+
+      * Informe o número do mês com 2 digitos.
+
+      * Este parâmetro pode ser usado em conjunto com o --inyear para limitar ao
+      mês de determinado ano.
+
+    --inyear, -Y [ANO]
+      Arquivos modificados no ano informado.
+
+      * Informe o número do ano com 4 digitos.
+
+      * Este parâmetro pode ser usado em conjunto com o --inmonth para limitar ao
+      mês de determinado ano.
+
+    --install
+      Instala o script em um diretório visivel pela variavel PATH para que o script
+      fique disponível a todos os usuarios do sistema.
+
+    --help, -h
+      Mostra esta ajuda
+
+    ex.:
+      find /var/log -type f \`findindate --today\` -ls
+      find /var/log -type f \`findindate --yesterday\` -ls
+      find /var/log -type f \`findindate --inmonth 05\` -ls
+      find /var/log -type f \`findindate --inyear 2018\` -ls
+      find /var/log -type f \`findindate --inyear 2016 --inmonth 02\` -ls
+      find /var/log -type f \`findindate --before 201705010000\` -ls
+      find /var/log -type f \`findindate --after 201705010000\` -ls
+
+2018 (C) - Welington R Braga
+EOF
+
+}
+
+YEAR=0
+MONTH=0
+DATA1=0
+DATA2=0
+informmonth=0
+informyear=0
+
+while [ "$1" ]; do
+
+  case "$1" in
+
+    "--between"|"-w" )
+
+      shift;
+      DATA1=$1;
+      shift;
+      DATA2=$1;
+      between $DATA1 $DATA2;
+
+    ;;
+
+    "--before"|"-b" )
+
+      shift;
+      DATA1=$1;
+      before $DATA1;
+
+    ;;
+
+    "--after"|"-a" )
+
+      shift;
+      DATA1=$1;
+      after $DATA1;
+
+    ;;
+
+    "--today" |"-t" )
+
+      DATA1=`date +%Y%m%d0000`;
+      after $DATA1;
+
+    ;;
+
+    "--yesterday"|"-y" )
+
+      DATA1=`date -d yesterday +%Y%m%d0000`;
+      DATA2=`date -d yesterday +%Y%m%d2359.59`;
+      between $DATA1 $DATA2;
+
+    ;;
+
+    "--inmonth"|"-m" )
+      informmonth=1;
+      shift;
+      MONTH=$1
+      checkdate
+      #Chamada ao termino do loop #between $DATA1 $DATA2;
+
+    ;;
+
+    "--inyear"|"-Y" )
+      informyear=1;
+      shift;
+      YEAR=$1
+      checkdate
+      #Chamada ao termino do loop #between $DATA1 $DATA2;
+
+    ;;
+
+    "--help" | "-h" )
+
+      help ;
+      exit 1;
+
+    ;;
+
+    "--teste-me" )
+      echo "Janeiro - 2001"
+      $0 --inyear 2001 --inmonth 01; ls -lh /tmp/*date
+      echo
+      echo "Fevereiro - 2002"
+      $0 --inmonth 02 --inyear 2002; ls -lh /tmp/*date
+      echo
+      echo "Março - deste ano"
+      $0 --inmonth 03; ls -lh /tmp/*date
+      echo
+      echo "Todo ano - 2004"
+      $0 --inyear 2004; ls -lh /tmp/*date
+      echo
+      echo "Ontem"
+      $0 --yesterday; ls -lh /tmp/*date
+      echo
+      echo "Hoje"
+      $0 --today; ls -lh /tmp/*date
+
+    exit 0;
+    ;;
+    "--install" )
+	if [ "$USER" != "root" ]; then
+	    echo "ERRO: A instalação deve ser feita pelo usuario root" >&2
+	    exit 1;
+	fi
+	FILE=`which findindate`
+	[ -f "$FILE" ] && rm "$FILE"
+	cp "$0" /usr/local/bin/findindate
+	chmod 0755 /usr/local/bin/findindate
+  esac
+  shift;
+
+done
+
+if (( $informyear == 1 )) || (( $informmonth == 1 )); then
+  between $DATA1 $DATA2;
+fi
+
+exit 0;
